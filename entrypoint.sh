0c3e4903f7bc5386150bd77b0318d6d2c8a39506
seyuf <mamadou@madit.fr>
2019-09-19 18:31:27 +0200

    removed sudo for testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 echo "hello your setup is $INPUT_PHP & $INPUT_PROCESS"
 
-sudo update-alternatives --set php /usr/bin/php${INPUT_PHP}
+update-alternatives --set php /usr/bin/php${INPUT_PHP}
 bash /opt/scripts/${INPUT_PROCESS}.sh
