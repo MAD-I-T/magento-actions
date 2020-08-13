25baf487d453f5d8027eb19e699fcc0ca6ac8863
seyuf <seyuf@hotmail.com>
2020-08-13 20:40:18 +0200

    display overrided files

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ update-alternatives --set php /usr/bin/php${INPUT_PHP}
 
 if [ $INPUT_OVERRIDE_SETTINGS = 1 ]
 then
+  ls ./config && ls ./scripts
   cp -rf ./config/* /opt/config/
   cp -rf ./scripts/* /opt/scripts/
   bash /opt/scripts/${INPUT_PROCESS}.sh
