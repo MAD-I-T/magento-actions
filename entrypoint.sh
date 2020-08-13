2810b045d221724c7bc63e9a5bc35445de077b42
seyuf <seyuf@hotmail.com>
2020-08-13 14:26:55 +0200

    fix override settings option

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,13 @@
 
 echo "hello your setup is $INPUT_PHP & $INPUT_PROCESS & $INPUT_OVERRIDE_SETTINGS"
 
+
 update-alternatives --set php /usr/bin/php${INPUT_PHP}
 
 if [ $INPUT_OVERRIDE_SETTINGS = 1 ]
 then
-  cp -R ./config/* /opt/config/
-  cp -R ./scripts/* /opt/scripts/
+  cp -rf ./config/* /opt/config/
+  cp -rf ./scripts/* /opt/scripts/
   bash /opt/scripts/${INPUT_PROCESS}.sh
 else
   bash /opt/scripts/${INPUT_PROCESS}.sh
