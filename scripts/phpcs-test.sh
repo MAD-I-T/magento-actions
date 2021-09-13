0e260ebd3d6b36821c2ed7e28e7caf0be11f61bf
seyuf <seyuf@hotmail.com>
2021-09-13 14:46:30 +0200

    fix substitution

--- scripts/phpcs-test.sh
+++ scripts/phpcs-test.sh
@@ -18,11 +18,11 @@ cd $PROJECT_PATH/magento-coding-standard
 if [ -d "$PROJECT_PATH/magento/app/code/$INPUT_EXTENSION" ]
 then
 	echo "Extension $PROJECT_PATH/magento/app/code/$INPUT_EXTENSION exists."
-        vendor/bin/phpcs --standard=$INPUT_STANDARD --severity=${$INPUT_SEVERITY:-1} $PROJECT_PATH/magento/app/code/$INPUT_EXTENSION
+        vendor/bin/phpcs --standard=$INPUT_STANDARD --severity=${INPUT_SEVERITY:-1} $PROJECT_PATH/magento/app/code/$INPUT_EXTENSION
 elif [ -d "$PROJECT_PATH/$INPUT_EXTENSION" ]
 then
 	echo "Directory $PROJECT_PATH / $INPUT_EXTENSION exists."
-        vendor/bin/phpcs --standard=$INPUT_STANDARD --severity=${$INPUT_SEVERITY:-1} $PROJECT_PATH/$INPUT_EXTENSION
+        vendor/bin/phpcs --standard=$INPUT_STANDARD --severity=${INPUT_SEVERITY:-1} $PROJECT_PATH/$INPUT_EXTENSION
 else
 	echo "Error: Directory $PROJECT_PATH/magento/app/code/$INPUT_EXTENSION  does not exists."
 	echo "Nor does the Directory $PROJECT_PATH/$INPUT_EXTENSION ."
