35a2aacfe59d38fc2c859a331440c3a4705d4004
seyuf <seyuf@hotmail.com>
2022-06-20 23:41:00 +0200

    fix magento install for version 2.4.x see #26

--- scripts/create-project.sh
+++ scripts/create-project.sh
@@ -44,7 +44,8 @@ then
            composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=${INPUT_MAGENTO_VERSION}
            ;;
         0|1|2|3)
-           php7.2 update-alternatives --set php /usr/bin/php7.4
+           php7.2 /usr/local/bin/composer self-update --2
+           update-alternatives --set php /usr/bin/php7.4
            composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=${INPUT_MAGENTO_VERSION}
            ;;
         *) echo "This version $INPUT_MAGENTO_VERSION of magento 2.4.X is not recognized minor $minorVersion" && exit 1 ;;
