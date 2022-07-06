628ac3ba3d5b3487ef8ef97b208b6f96f74f6ecf
seyuf <seyuf@hotmail.com>
2022-07-07 01:02:09 +0200

    pwa-studio installer

--- scripts/pwa-studio-install.sh
+++ scripts/pwa-studio-install.sh
@@ -3,12 +3,20 @@
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install $INPUT_NODE_VERSION
+nvm install 16
 npm install --location=global yarn
-yarn install  && yarn add compression
+yarn install && yarn add compression
+
 
 set -x
-npx -y ${INPUT_VERSION:+'--force'} @magento/pwa-buildpack create-project ./pwa-studio --name @madit/pwa-studio --template @magento/venia-concept${INPUT_VERSION:+"@"}$INPUT_VERSION --backend-url https://master-7rqtwti-mfwmkrjfqvbjk.us-4.magentosite.cloud --backend-edition MOS --braintree-token sandbox_8yrzsvtm_s2bg8fs563crhqzk --author Madit
+yarn add @magento/pwa-buildpack
+yarn add @magento/pwa
+
+yarn cache clear
+yarn buildpack  create-project   ./pwa-studio --name @madit/pwa-studio --template @magento/venia-concept --backend-url https://master-7rqtwti-mfwmkrjfqvbjk.us-4.magentosite.cloud --backend-edition MOS --braintree-token sandbox_8yrzsvtm_s2bg8fs563crhqzk --author Madit
+
+
+set -x
 
 
 ls -ltah
