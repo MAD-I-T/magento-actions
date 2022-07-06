7be94febce9de8be3d6ffa52ec69a55fb577e0fa
seyuf <seyuf@hotmail.com>
2022-07-06 19:03:51 +0200

    fix pwa-studio installer

--- scripts/pwa-studio-install.sh
+++ scripts/pwa-studio-install.sh
@@ -7,16 +7,20 @@ nvm install $INPUT_NODE_VERSION
 npm install --global yarn
 yarn install  && yarn add compression
 
+set -x
 npx -y ${INPUT_VERSION:+'--force'} @magento/pwa-buildpack create-project ./pwa-studio --name @madit/pwa-studio --template @magento/venia-concept${INPUT_VERSION:+"@"}$INPUT_VERSION --backend-url https://master-7rqtwti-mfwmkrjfqvbjk.us-4.magentosite.cloud --backend-edition MOS --braintree-token sandbox_8yrzsvtm_s2bg8fs563crhqzk --author Madit
 
+
+ls -ltah
+pwd
 
 if [ "$INPUT_NO_PUSH" -ne 1 ]
 then
   git config user.name github-actions
   git config user.email github-actions@github.com
-  git add pwa-studio/.gitignore
+  git add ./pwa-studio/.gitignore
   git commit -m 'added pwa-studio gitignore'
-  git add pwa-studio
+  git add ./pwa-studio
   git commit -m "add pwa-studio project to github repo"
   git push
 fi
