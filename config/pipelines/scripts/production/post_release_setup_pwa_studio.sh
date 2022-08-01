5c889669c11ede20c908127f4ffd77bca171f3ff
Mamadou <seyuf@hotmail.com>
2022-08-01 12:33:04 +0200

    Update post_release_setup_pwa_studio.sh

--- config/pipelines/scripts/production/post_release_setup_pwa_studio.sh
+++ config/pipelines/scripts/production/post_release_setup_pwa_studio.sh
@@ -1,3 +1,27 @@
 #!/usr/bin/env bash
+
 echo "post release script pwa studio start"
-yarn start
+
+isNpmPackageInstalled() {
+  sudo npm list --depth 1 -g $1 > /dev/null 2>&1
+}
+
+# checks if pm2 is installed
+if isNpmPackageInstalled "pm2"
+then
+  echo "pm2 is already installed"
+else
+  echo "pm2 is NOT installed please install it on the server and redeploy"
+  echo "use sudo yarn global add  pm2 \n or \n sudo npm install -g pm2"
+  #sudo yarn global add  pm2
+  #npm install pm2
+fi
+
+# get into the dist dir to start prod server
+cd dist
+
+echo "Start the node server monnitored by PM2"
+
+pm2 stop pwa_studio
+pm2 delete pwa_studio
+pm2 start yarn --name pwa_studio -- start
