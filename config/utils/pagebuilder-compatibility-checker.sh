7d4f55ab4bbcdb1f65180d29a1716feadf51c84c
Mamadou <seyuf@hotmail.com>
2022-09-20 17:21:32 +0200

    Fixes #11 and #30

--- config/utils/pagebuilder-compatibility-checker.sh
+++ config/utils/pagebuilder-compatibility-checker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ ! -f magento/app/etc/config.php ]
+if [ ! -f app/etc/config.php ]
 then
   echo "Generating config.php if does not exist | fix for magento/magento2-page-builder#730"
   bin/magento module:enable --all
