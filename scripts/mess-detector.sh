94ba03620ec90e7de03dc18203d558505671a0c9
seyuf <seyuf@hotmail.com>
2022-12-16 19:00:16 +0100

    mess detector composer install

--- scripts/mess-detector.sh
+++ scripts/mess-detector.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 
-set -e
-
 PROJECT_PATH="$(pwd)"
-
 
 echo "currently in $PROJECT_PATH"
 
 cd "$PROJECT_PATH/magento"
+
+
+/usr/local/bin/composer install --dry-run --prefer-dist --no-progress &> /dev/null
+
+COMPOSER_COMPATIBILITY=$?
+
+echo "Composer compatibility: $COMPOSER_COMPATIBILITY"
+
+set -e
+
+if [ $COMPOSER_COMPATIBILITY = 0 ]
+then
+	/usr/local/bin/composer install --prefer-dist --no-progress
+else
+  echo "using composer v1"
+  php7.2 /usr/local/bin/composer self-update --1
+	/usr/local/bin/composer install --prefer-dist --no-progress
+fi
 
 
 if [ ! -f $INPUT_RULESET ]
