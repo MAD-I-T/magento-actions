22d43200d55032200ae23fbc27392ab1f60a5751
seyuf <seyuf@hotmail.com>
2022-12-16 18:38:41 +0100

    limit md to magento projects only

--- scripts/mess-detector.sh
+++ scripts/mess-detector.sh
@@ -9,8 +9,6 @@ echo "currently in $PROJECT_PATH"
 
 cd "$PROJECT_PATH/magento"
 
-wget -c https://phpmd.org/static/latest/phpmd.phar
-
 
 if [ ! -f $INPUT_RULESET ]
 then
@@ -20,13 +18,12 @@ then
   INPUT_RULESET=ruleset.xml
 fi
 
+
 if [ -n $INPUT_MD_SRC_PATH ]
 then
-  	echo -e "\e[32mMess detection initiated\e[0m"
-    php phpmd.phar $INPUT_MD_SRC_PATH text $INPUT_RULESET
+  echo -e "\e[32mMess detection initiated\e[0m"
+  php vendor/phpmd/phpmd/src/bin/phpmd $INPUT_MD_SRC_PATH ansi $INPUT_RULESET
 else
   echo -e "\e[31mPlease specify the $md_src_path\e[0m"
   exit 1;
 fi
-
-
