7b5c343c5fd5a16939c26407b4b936fd7dfa09c1
seyuf <seyuf@hotmail.com>
2023-03-15 11:46:49 +0100

    auto search compatibility checker

---
+++ config/utils/search-engine-compatibility-checker.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+
+MAGE_VERSION=$(grep -ni '"version"' magento/composer.json | grep -o -E '\:\ .+' | cut -d' ' -f6 | cut -d',' -f1 | cut -d'"' -f2;);
+
+
+if [ -z "$MAGE_VERSION" ]
+then
+  MAGE_VERSION=$(grep -ni '"version"' ./magento/composer.json | grep -o -E '\:\ .+' | cut -d ' ' -f4 | cut -d',' -f1 | cut -d'"' -f2;);
+fi
+
+set -e
+echo "Current magento version is $MAGE_VERSION"
+pwd
+ls -lath magento/composer.json
+
+
+majorVersion=${MAGE_VERSION:2:1}
+minorVersion=${MAGE_VERSION:4:1}
+
+if [ -n "$MAGE_VERSION" ]
+then
+  case "$majorVersion" in
+    2)
+         echo "No search engine needed";
+         ;;
+    3)case "$minorVersion" in
+        4|5|6|7|8)
+           echo "switching search engine to elasticsearch";
+           [ -z "$INPUT_OPENSEARCH"  -a  -z  "$INPUT_ELASTICSEARCH" ] && echo $'INPUT_ELASTICSEARCH=1 \nINPUT_OPENSEARCH=0' >> /etc/environment;
+           ;;
+        0|1|2|3)
+           echo "switching search engine to elasticsearch";
+           [ -z "$INPUT_OPENSEARCH"  -a  -z  "$INPUT_ELASTICSEARCH" ] && echo $'INPUT_ELASTICSEARCH=1 \nINPUT_OPENSEARCH=0' >> /etc/environment;
+           ;;
+        *) echo "This version $INPUT_MAGENTO_VERSION of magento 2.4.X is not recognized minor $minorVersion" ;;
+      esac ;;
+    4)
+      case "$minorVersion" in
+        6)
+           echo "switching search engine to opensearch";
+           [ -z "$INPUT_OPENSEARCH"  -a  -z  "$INPUT_ELASTICSEARCH" ] && echo $'INPUT_ELASTICSEARCH=0 \nINPUT_OPENSEARCH=1' >> /etc/environment;
+           ;;
+        4|5)
+           echo "switching search engine to elasticsearch";
+           [ -z "$INPUT_OPENSEARCH"  -a  -z  "$INPUT_ELASTICSEARCH" ] && echo $'INPUT_ELASTICSEARCH=1 \nINPUT_OPENSEARCH=0' >> /etc/environment;
+           ;;
+        0|1|2|3)
+           echo "switching search engine to elasticsearch";
+           [ -z "$INPUT_OPENSEARCH"  -a  -z  "$INPUT_ELASTICSEARCH" ] && echo $'INPUT_ELASTICSEARCH=1 \nINPUT_OPENSEARCH=0' >> /etc/environment;
+           ;;
+        *) echo "This version $MAGE_VERSION of magento 2.4.X is not recognized minor $minorVersion";;
+      esac ;;
+    *) echo "This version $MAGE_VERSION of magento is not recognized minor $minorVersion"  ;;
+   esac
+else
+  echo "No match found"
+fi
--- entrypoint.sh
+++ entrypoint.sh
@@ -9,13 +9,23 @@ if [ $INPUT_PHP = 'auto' ]
 then
   bash /opt/config/utils/php-compatibility-checker.sh
 else
-  echo "Forcing php to match specified php inputs argument"
+  echo "Forcing php to match specified input argument"
   update-alternatives --set php /usr/bin/php${INPUT_PHP}
+fi
+
+if [ -z "$INPUT_OPENSEARCH"  -a  -z  "$INPUT_ELASTICSEARCH" ]
+then
+  bash /opt/config/utils/search-engine-compatibility-checker.sh
+else
+  echo "Forcing search engine to match specified input argument"
+  echo "Elastisearch: $INPUT_ELASTICSEARCH"
+  echo "Opensearch: $INPUT_Opensearch"
 fi
 
 
 if [ "$INPUT_COMPOSER_VERSION" -ne 0 ]
 then
+  echo "Forcing composer to match specified input argument"
   php7.2 /usr/local/bin/composer self-update --${INPUT_COMPOSER_VERSION}
 fi
 
