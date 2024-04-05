0e9e6a9ebc8252e51fa9983111eaeca58c34b251
Mamadou <seyuf@hotmail.com>
2024-04-05 17:15:53 +0200

    Update post_release_cleanup.sh

--- config/pipelines/scripts/production/post_release_cleanup.sh
+++ config/pipelines/scripts/production/post_release_cleanup.sh
@@ -8,8 +8,11 @@ echo "cleaning up buckets"
 
 KEEP_RELEASE_NR="NR>$1";
 
-
 rm -f `ls -t | awk $KEEP_RELEASE_NR`
+ls -thla
 
 cd ../releases/
+echo "cleaning up releases"
+
 rm -rf `ls -t | awk $KEEP_RELEASE_NR`
+ls -thla
