98f0b0806de4f1934db0abd45d1febe4ee364f49
Mamadou <seyuf@hotmail.com>
2024-05-14 17:41:42 +0200

    prevent pipeline errors when pm2 is not installed

--- config/pipelines/scripts/staging/post_release_setup_pwa_studio.sh
+++ config/pipelines/scripts/staging/post_release_setup_pwa_studio.sh
@@ -5,7 +5,7 @@ echo "post release script pwa studio start"
 isNpmPackageInstalled() {
   sudo npm list --depth 1 -g $1 > /dev/null 2>&1
 }
-
+set +e
 # checks if pm2 is installed
 if isNpmPackageInstalled "pm2"
 then
@@ -25,4 +25,4 @@ echo "Start the node server monnitored by PM2"
 pm2 stop pwa_studio
 pm2 delete pwa_studio
 pm2 start yarn --interpreter bash --name pwa_studio -- start
-
+set -e
