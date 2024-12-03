75aa12fc3090800a11a55548e46e6325d99b7da9
Mamadou <seyuf@hotmail.com>
2024-12-03 19:31:51 +0100

    added composer audit task

---
+++ scripts/security-composer-audit.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+PROJECT_PATH="$(pwd)"
+
+echo "currently in $PROJECT_PATH"
+
+cd "$PROJECT_PATH/magento"
+
+/usr/local/bin/composer install --dry-run --prefer-dist --no-progress &> /dev/null
+
+COMPOSER_COMPATIBILITY=$?
+
+echo "Composer compatibility: $COMPOSER_COMPATIBILITY"
+
+
+set -e
+
+if [ $COMPOSER_COMPATIBILITY = 0 ]
+then
+	/usr/local/bin/composer install --prefer-dist --no-progress
+        /usr/local/bin/composer audit
+else 
+  echo "using composer v1 audit is not available"
+fi
